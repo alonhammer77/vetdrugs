ccd5aa8d53db710e331d123551e5b8c960aa421e
Alon Hammer <alonhammer77@users.noreply.github.com>
2025-10-20 10:21:25 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-  base: './', // safe for static hosting (important for Netlify/Vercel)
+  base: '/vetdrugs/'
+
 })
